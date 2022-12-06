6d03314cc36b9d7aeaa3f4f3238d32e3e15938a2
silloky <ek.mindstorms@outlook.com>
2022-12-06 11:15:35 +0100

    ryhgfzieuysdhx

--- db.php
+++ db.php
@@ -4,10 +4,10 @@
     $password = "***REMOVED***";
     $database = "***REMOVED***_donations";
     $port = "3306";
-    $connection = new mysqli($servername, $username, $password, $database, $port);
+    $connection = mysqli_connect($servername, $username, $password, $port);
+    mysqli_select_db($connection, $database);
     // Check connection
     if ($connection->connect_error) {
       die("Connection failed: " . $connnection->connect_error);
     }
-    echo "connection ok"
 ?>
