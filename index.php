98bce9c1b94dcc0d895ba6a4e26a8e9f1b832ccc
silloky <ek.mindstorms@outlook.com>
2022-12-06 13:34:33 +0100

    ezrtg(ref

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
     $result = mysqli_query($conn, 'SELECT SUM(amount_donated) AS value_sum FROM donations'); 
     $row = mysqli_fetch_assoc($result); 
     $sum = $row['value_sum'];
-    echo $sum
+    echo "this is te sum : $sum"
 ?>
