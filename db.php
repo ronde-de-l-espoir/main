c09301191b87d52134cf7cc744b99c5213172b24
silloky <ek.mindstorms@outlook.com>
2022-12-06 11:15:35 +0100

    ryhgfzieuysdhx

--- db.php
+++ db.php
@@ -4,10 +4,10 @@
     $password = "o2switch2022";
     $database = "ctzs1179_donations";
     $port = "3306";
-    $connection = new mysqli($servername, $username, $password, $database, $port);
+    $connection = mysqli_connect($servername, $username, $password, $port);
+    mysqli_select_db($connection, $database);
     // Check connection
     if ($connection->connect_error) {
       die("Connection failed: " . $connnection->connect_error);
     }
-    echo "connection ok"
 ?>
