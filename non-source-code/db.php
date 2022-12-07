62dbc87a2fbf1c0cb910a16cb00fd5dec992303d
silloky <ek.mindstorms@outlook.com>
2022-12-07 13:42:00 +0100

    mysql integration in php

--- non-source-code/db.php
+++ non-source-code/db.php
@@ -1,13 +1,8 @@
 <?php
-    $servername = "localhost";
     $username = "ctzs1179";
     $password = "o2switch2022";
-    $database = "ctzs1179_donations";
-    $port = "3306";
-    $connection = mysqli_connect($servername, $username, $password, $port);
-    mysqli_select_db($connection, $database);
-    // Check connection
-    if ($connection->connect_error) {
-      die("Connection failed: " . $connnection->connect_error);
-    }
+    $dsn = "mysql:host=localhost;dbname=ctzs1179_donations";
+    $user = "ctzs1179";
+    $passwd = "o2switch2022";
+    $pdo = new PDO($dsn, $user, $passwd);
 ?>
--- non-source-code/index.php
+++ non-source-code/index.php
@@ -1,7 +1,7 @@
 <?php
     include_once "./db.php";
-    $result = mysqli_query($conn, 'SELECT SUM(amount_donated) AS value_sum FROM donations'); 
-    $row = mysqli_fetch_assoc($result); 
-    $sum = $row['value_sum'];
-    echo "this is te sum : $sum"
+    $sql = 'SELECT SUM(amount_donated) AS value_sum FROM donations';
+    $stm = $pdo->query($sql);
+    $result = $stm->fetch();
+    echo $result[0];
 ?>
