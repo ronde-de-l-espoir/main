4697a967d6d76c100e2b3ee44400ecf7500512cc
silloky <ek.mindstorms@outlook.com>
2022-12-07 13:50:16 +0100

    deleted php from main

--- non-source-code/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once "./db.php";
-    $result = mysqli_query($conn, 'SELECT SUM(amount_donated) AS value_sum FROM donations'); 
-    $row = mysqli_fetch_assoc($result); 
-    $sum = $row['value_sum'];
-    echo "this is te sum : $sum"
-?>
