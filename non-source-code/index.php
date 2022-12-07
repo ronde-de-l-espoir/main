8868bfc6ddf5d37101fec3041d1c0b156b7ee32a
silloky <ek.mindstorms@outlook.com>
2022-12-07 16:15:53 +0100

    added php

--- non-source-code/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once "./db.php";
-    $sql = 'SELECT SUM(amount_donated) AS value_sum FROM donations';
-    $stm = $pdo->query($sql);
-    $result = $stm->fetch();
-    echo $result[0];
-?>
