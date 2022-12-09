ba98ec412798811ca659f5c373364fcf0223b072
Elias Kirkwood <yannis3736@gmail.com>
2022-12-10 00:37:39 +0100

    Comment out some code I tried.

--- config.php
+++ config.php
@@ -1,9 +1,16 @@
 <?php
-    // $username = "***REMOVED***";
-    // $password = "***REMOVED***";
-    // $dsn = "mysql:host=localhost;dbname=***REMOVED***_donations";
-    // $user = "***REMOVED***";
-    // $passwd = "***REMOVED***";
-    // $pdo = new PDO($dsn, $user, $passwd);
-    $conn = mysqli_connect('mysql:host=localhost;dbname=***REMOVED***_donations', '***REMOVED***', '***REMOVED***', '***REMOVED***_donations');
+    $username = "***REMOVED***";
+    $password = "***REMOVED***";
+    $dsn = "mysql:host=localhost;dbname=***REMOVED***_donations";
+    $user = "***REMOVED***";
+    $passwd = "***REMOVED***";
+    $pdo = new PDO($dsn, $user, $passwd);
+    // $host = 'ronde-de-l-espoir.fr';
+    // echo $host;
+
+    // $conn = mysqli_connect($host, '***REMOVED***_***REMOVED***', '***REMOVED***', '***REMOVED***_donations', 3306);
+
+    // if (!$conn) {
+    //     echo "Connection error: " . mysqli_connect_error();
+    // };
 ?>
