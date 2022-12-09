0cecbd151f05bf0b6aaea8f4fed6743cf8ae3921
Elias Kirkwood <yannis3736@gmail.com>
2022-12-09 23:29:12 +0100

    Testing PHP + renamed the config of php connection.

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    // $username = "ctzs1179";
+    // $password = "o2switch2022";
+    // $dsn = "mysql:host=localhost;dbname=ctzs1179_donations";
+    // $user = "ctzs1179";
+    // $passwd = "o2switch2022";
+    // $pdo = new PDO($dsn, $user, $passwd);
+    $conn = mysqli_connect('mysql:host=localhost;dbname=ctzs1179_donations', 'ctzs1179', 'o2switch2022', 'ctzs1179_donations');
+?>
