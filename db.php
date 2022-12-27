e7b900643ec5d11b3bbda99e16f3dc4f5b0f3ea3
Skyman-2 <yannis3736@gmail.com>
2022-12-27 15:15:29 +0100

    Reinstate PHP conn with host address

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     $username = "ctzs1179";
     $password = "o2switch2022";
-    $dsn = "mysql:host=localhost;dbname=ctzs1179_donations";
+    $dsn = "mysql:host=ronde-de-l-espoir.fr;dbname=ctzs1179_donations";
     $user = "ctzs1179";
     $passwd = "o2switch2022";
     $pdo = new PDO($dsn, $user, $passwd);
--- index.php
+++ index.php
@@ -38,13 +38,13 @@
 
     <main>
         <div class="count-box">
-            <!-- <?php
+            <?php
                 include_once "./db.php";
                 $sql = 'SELECT SUM(amount_donated) AS value_sum FROM donations';
                 $stm = $pdo->query($sql);
                 $result = $stm->fetch();
                 echo $result[0];
-            ?> -->
+            ?>
         </div>
     </main>
 
