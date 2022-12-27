2c075354d3423e9c8612efe1999bd41e79cf6ffc
Skyman-2 <yannis3736@gmail.com>
2022-12-27 15:15:29 +0100

    Reinstate PHP conn with host address

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     $username = "***REMOVED***";
     $password = "***REMOVED***";
-    $dsn = "mysql:host=localhost;dbname=***REMOVED***_donations";
+    $dsn = "mysql:host=ronde-de-l-espoir.fr;dbname=***REMOVED***_donations";
     $user = "***REMOVED***";
     $passwd = "***REMOVED***";
     $pdo = new PDO($dsn, $user, $passwd);
