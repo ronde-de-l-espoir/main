8b56bc15c9aa268cd8c38aab21a4362954840fa7
Skyman-2 <yannis3736@gmail.com>
2022-12-27 19:15:29 +0100

    Feat ; Add gallery page.

--- index.php
+++ index.php
@@ -8,16 +8,16 @@ if($_SERVER["HTTPS"] != "on")
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>La Ronde de l'Espoir</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>La Ronde de l'Espoir</title>
-    <link rel="shortcut icon" href="img/LRDE-logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;800;900&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="img/LRDE-logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
     <script src="app.js" defer></script>
 </head>
 <body>
@@ -33,7 +33,7 @@ if($_SERVER["HTTPS"] != "on")
                 <div class="link-wrapper">
                     <a href="#" class="underline-anim" data-isFocus="true">Accueil</a>
                     <a href="./infos/" class="underline-anim" data-isFocus="false">Infos</a>
-                    <a href="#" class="underline-anim" data-isFocus="false">Galerie</a>
+                    <a href="./galerie/" class="underline-anim" data-isFocus="false">Galerie</a>
                     <a href="./donation/" id="donate-btn" data-isFocus="false">Faire un don</a>  <!-- C'est plus pratique de rajouter un "./" devant les liens. -->
                 </div>
             </div>
