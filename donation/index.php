27c366dd14e9f21aca1d6f163f38d823e8a23c67
Skyman-2 <yannis3736@gmail.com>
2022-12-28 21:35:13 +0100

    Feat : Added a temporary "Go Back" button

--- donation/index.php
+++ donation/index.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <style>
+        a {
+            width: fit-content;
+        }
+    </style>
     <title>Y a du PhP!!!</title>
 </head>
 <body>
@@ -12,5 +18,7 @@
 
     <?php echo "Yeah well in this discussion I might be bold, but I'm a bold high-grounder" ?>
     
+    <a href="../" class="btn-grad">Go Back</a>
+
 </body>
 </html>
