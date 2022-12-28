2a7901a58a3a8043792eacc0f4ccf4dd9ad8314f
Skyman-2 <yannis3736@gmail.com>
2022-12-28 20:06:32 +0100

    Fix : PHP and SQL link now working

--- db.php
+++ db.php
@@ -1,24 +1,18 @@
 <?php
-    // $dsn = "mysql:host=ronde-de-l-espoir.fr;dbname=***REMOVED***_donations";
-    // $user = "***REMOVED***";
-    // $passwd = "***REMOVED***";
-
-    // try { 
-    //     $pdo = new PDO($dsn, $user, $passwd);
-    // } catch (PDOException $e) {
-    //     echo $e;
-    //     die();
-    // }
 
     $servername = "ronde-de-l-espoir.fr";
     $username = "***REMOVED***";
     $password = "***REMOVED***";
     $dbname = "***REMOVED***_donations";
 
-    $conn = new mysqli($servername, $username, $password);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    echo "Operation is a success.";
+
+    $sql = 'SELECT SUM(amount_donated) AS value_sum FROM donations';
+    $results = mysqli_fetch_all(mysqli_query($conn, $sql));
+    $totalDonations = $results[0][0];
+
 ?>
