8a6c1e10605bdea07131c0b42fb49adc3231fcb0
Skyman-2 <yannis3736@gmail.com>
2022-12-30 20:45:11 +0100

    Fix : Relocated some code.

--- db_config.php
+++ db_config.php
@@ -7,25 +7,4 @@
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    $sql = 'SELECT SUM(amount_donated) AS value_sum FROM donations';
-    $results = mysqli_fetch_all(mysqli_query($conn, $sql));
-    $totalDonations = round($results[0][0], 2);
-    
-    $totalDonations = sprintf("%.2f", $totalDonations);
-
-    for ($i = 0; $i < strlen($totalDonations); $i++ ) {
-        if ($totalDonations[$i] == ".") {
-            $totalDonations[$i] = ",";
-        }
-    }
-
-    $sql = 'SELECT COUNT(id) as count_participants FROM donations';
-    $results = mysqli_fetch_all(mysqli_query($conn, $sql));
-
-    $totalParticipants = $results[0][0];
-
 ?>
