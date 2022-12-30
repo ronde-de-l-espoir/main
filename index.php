64bbcd5d198b990519e7617307a3e0d349dbcdf6
Skyman-2 <yannis3736@gmail.com>
2022-12-30 20:45:11 +0100

    Fix : Relocated some code.

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <?php
 
-if($_SERVER["HTTPS"] != "on") {
-    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-    exit();
-}
+    if($_SERVER["HTTPS"] != "on") {
+        header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+        exit();
+    }
 
-include('./db_config.php');
+    include('./db_config.php');
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = 'SELECT SUM(amount_donated) AS value_sum FROM donations';
+    $results = mysqli_fetch_all(mysqli_query($conn, $sql));
+    $totalDonations = round($results[0][0], 2);
+    
+    $totalDonations = sprintf("%.2f", $totalDonations);
+
+    for ($i = 0; $i < strlen($totalDonations); $i++ ) {
+        if ($totalDonations[$i] == ".") {
+            $totalDonations[$i] = ",";
+        }
+    }
+
+    $sql = 'SELECT COUNT(id) as count_participants FROM donations';
+    $results = mysqli_fetch_all(mysqli_query($conn, $sql));
+
+    $totalParticipants = $results[0][0];
 
 ?>
 
