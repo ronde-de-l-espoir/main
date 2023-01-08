2a3517adac9f671533183b88e940c65664400df6
Elias Kirkwood <yannis3736@gmail.com>
2023-01-08 18:55:40 +0100

    Fix : Gallery now has CSS corrected.

--- galerie/index.php
+++ galerie/index.php
@@ -10,7 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;800;900&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="../img/LRDE-logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../common.css">
     <script src="../app.js" defer></script>
 </head>
 <body>
