a7d7eb14b1c9f7b496564f4ddea3e44897eb1cdb
silloky <ek.mindstorms@outlook.com>
2023-01-09 19:51:51 +0100

    tried to include footer in main + footer css

---
+++ footer.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./common.css">
+</head>
+<body>
+    <div class="footer-wrapper">
+        <link rel="stylesheet" href="./common.css">
+        <div style="float: left;">
+            <p>Les onglets</p>
+            <a>Bla</a>
+            <a>Bla</a>
+            <a>Bla</a>
+            <a>Bla</a>
+            <a>Bla</a>
+            <a>Bla</a>
+        </div>
+        <div>
+            <p>Les onglets</p>
+            <a>Bla</a>
+            <a>Bla</a>
+        </div>
+        <div>
+            <img src="./img/clean_logo_littoral.png" id="footer-img">
+        </div>
+    </div>
+</body>
+</html>
