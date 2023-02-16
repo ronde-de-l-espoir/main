b21c31ce80624c3af007fe20dfddf69b854db857
Silloky <ek.mindstorms@outlook.com>
2023-02-16 18:11:13 +0100

    Refactor : removed unimportant php

--- get_total.php
+++ get_total.php
@@ -12,12 +12,6 @@
     $bareTotal = $results[0][0];
     $totalDonations = intval($results[0][0]);
 
-    for ($i = 0; $i < strlen($totalDonations); $i++ ) {
-        if ($totalDonations[$i] == ".") {
-            $totalDonations[$i] = ",";
-        }
-    }
-
     $sql = 'SELECT COUNT(id) as count_participants FROM donations';
     $results = mysqli_fetch_all(mysqli_query($conn, $sql));
 
