bc62f687a4b945fabedf1cf8046aa2c9dc2bf052
Elias Kirkwood <yannis3736@gmail.com>
2023-03-23 20:01:45 +0100

    Fix : Page title on cookies page

--- cookies/index.php
+++ cookies/index.php
@@ -10,7 +10,7 @@
 <html lang="en" data-hidden>
 
 <head>
-    <title>Cookies - LRDE</title>
+    <title>Cookies - La Ronde de l'Espoir</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
