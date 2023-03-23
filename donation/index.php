ce1c7b3dc6912fd8e91efbe5b4af29de34cfc513
Elias Kirkwood <yannis3736@gmail.com>
2023-03-23 20:03:19 +0100

    Fix : Page title on donation page

--- donation/index.php
+++ donation/index.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html lang="en" data-hidden>
 <head>
-    <title>La Ronde de l'Espoir - Faire un don</title>
+    <title>Faire un don - La Ronde de l'Espoir</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
