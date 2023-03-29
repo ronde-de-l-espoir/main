3ddb31a48b0abf75479807ba19e0c510b4cf52cf
Silloky <ek.mindstorms@outlook.com>
2023-03-29 20:21:25 +0200

    Started article

--- infos/articles.php
+++ infos/articles.php
@@ -6,6 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">    
 </head>
 <body>
+    <header>
+        <div class="header-wrapper">
+            
+            <span class="title">La Ronde de l'Espoir</span>
+            <span class="arrow">
+                <a href="https://ronde-de-l-espoir.fr/infos">
+                    <span class="material-symbols-outlined">arrow_back</span>
+                </a>
+            </span>
+            
+            <span class="nav-image">
+                <img src="../img/" alt="">
+            </span>
+        </div>
+    </header>
     <p id="hello"></p>
 </body>
 <script src="../articles.js"></script>
