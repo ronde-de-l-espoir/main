16cc8f8764a95028f79345d8a18ba9769f6b8010
Elias Kirkwood <ek.mindstorms@outlook.com>
2023-04-01 12:51:03 +0200

    Work on JS

--- infos/articles.js
+++ infos/articles.js
@@ -1,16 +1,54 @@
 var articlesJSON = []
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", '../articles.json', false);
-xhr.onreadystatechange = function () {
-	if (xhr.readyState === 4) {
-		if (xhr.status === 200 || xhr.status == 0) {
-			var articlesJSONText = xhr.response;
-			articlesJSON = JSON.parse(articlesJSONText)
-		}
-	}
-}
-xhr.send(null)
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", '../articles.json', false);
+// xhr.onreadystatechange = function () {
+// 	if (xhr.readyState === 4) {
+// 		if (xhr.status === 200 || xhr.status == 0) {
+// 			var articlesJSONText = xhr.response;
+// 			articlesJSON = JSON.parse(articlesJSONText)
+// 		}
+// 	}
+// }
+// xhr.send(null)
+
+const URL = 'https://docs.google.com/spreadsheets/d/14wG0Ywy1BxvxptXbvi7smP4fv6kOcQyyt5Yh0TL3OXE/gviz/tq?tqx=out:json'
+
+fetch(URL)
+	.then(response => function(){
+		const responseText = response.text();
+		return responseText
+	}).then(function(responseText){
+		console.log(responseText)
+		let json_string = responseText.substring(47).slice(0, -2);
+		return json_string
+	}).then(function(){
+		let parsed = JSON.parse(json_string)
+		return parsed
+	}).catch(err => console.error(err));
+
+	
+
+// async function getSheet (){
+// 	const captionsURL = 'https://docs.google.com/spreadsheets/d/1l6PfRKvyKB042l8pC-WhLzxSRt4YrqNbtwGudRe4Jjo/gviz/tq?tqx=out:json'
+// 	const fullResponse = await fetch(captionsURL)
+// 	const textResponse = await fullResponse.text()
+// 	let json_string = textResponse.substring(47).slice(0, -2);
+// 	let parsed = await JSON.parse(json_string);
+// 	return parsed;
+// }
+
+// async function sheetToArray(params) {
+// 	// var captions = [];
+// 	parsed = await getSheet();
+// 	parsed.table.rows.forEach((row) => {
+// 		let caption = []
+// 		caption.push(row.c[0].v)
+// 		caption.push(row.c[1].v)
+// 		captions.push(caption)
+// 	})
+// }
+
 
 var currentPage = window.location.pathname.split('/').slice(-2, -1)[0]
 
