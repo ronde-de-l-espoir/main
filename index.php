7a6a81fc2fff0689bb0a0bf96dfa7f3ceec6b75e
Elias Kirkwood <yannis3736@gmail.com>
2023-04-02 18:22:46 +0200

    Add layout of carousel

--- index.php
+++ index.php
@@ -14,10 +14,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    
+    
     <link rel="shortcut icon" href="img/LRDE-logo.png" type="image/x-icon">
     <link rel="stylesheet" href="common.css">
     <link rel="stylesheet" href="accueil-style.css">
@@ -101,7 +105,21 @@
             </div>
 
         </div>
+
+        <div class="carousel">
+            <div class="swipe" id="left">
+                <div class="swipe-arrow"><span class="material-symbols-outlined">arrow_back</span></div>
+            </div>
+            <div class="carousel-image"></div>
+            <div class="swipe" id="right">
+                <div class="swipe-arrow"><span class="material-symbols-outlined">arrow_forward</span></div>
+            </div>
+        </div>
+
     </main>
+
+    <br>
+    <br>
 
     <footer>
         <?php 
