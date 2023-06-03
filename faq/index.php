72a6a112901e5ce5d7ac8530e14d8312aede6877
Elias Kirkwood <94617006+Silloky@users.noreply.github.com>
2023-06-03 08:14:56 +0200

    Revert "End"

--- faq/index.php
+++ faq/index.php
@@ -37,6 +37,7 @@
                     <a href="#" class="underline-anim" data-isFocus="true">Accueil</a>
                     <a href="../infos/" class="underline-anim" data-isFocus="false">Infos</a>
                     <a href="../galerie/" class="underline-anim" data-isFocus="false">Galerie</a>
+                    <a href="../donation/" id="donate-btn" data-isFocus="false">Faire un don</a>  <!-- C'est plus pratique de rajouter un "./" devant les liens. -->
                 </div>
             </div>
         </nav>
@@ -58,6 +59,7 @@
                 <div class="separation" data-hidden></div>
             </div>
 
+            <a href="../donation/" id="side-nav-donation" class="btn-grad">Faites un don!</a>
             
         </div>
         
