3c192965bf3ab9dd63c73b3655f67ef49911e690
Elias Kirkwood <ek.mindstorms@outlook.com>
2023-06-04 13:51:54 +0200

    Feat : mention-legales formatting

--- mentions-legales/index.php
+++ mentions-legales/index.php
@@ -3,6 +3,10 @@
 //     header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
 //     exit();
 // }
+
+// THE BEGINNING OF THIS PAGE IS THE SAME AS THE HOME PAGE : THE COMMENTS ARE THE SAME
+
+
 ?>
 
 
@@ -27,12 +31,16 @@
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-6XM6CLQWYR"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    window.dataLayer = window.dataLayer || [];
 
-  gtag('config', 'G-6XM6CLQWYR');
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-6XM6CLQWYR');
 </script>
+
 <body data-hidden>
 
     <header data-hidden>
@@ -81,45 +89,57 @@
                 <p>En vigueur au 02/04/2023</p>
             </div>
             <p>
-            Conformément aux dispositions des Articles 6 et 19 de la Loi n°2004-575 du 21 juin 2004 pour la Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et visiteurs, ci-après l' "Utilisateur", du site https://ronde-de-l-espoir.fr/, ci-après le "Site", les présentes mentions légales.
-<br>La connexion et la navigation sur le Site par l’Utilisateur implique l’acceptation intégrale et sans réserve des présentes mentions légales. 
+                Conformément aux dispositions des Articles 6 et 19 de la Loi n°2004-575 du 21 juin 2004 pour la Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et visiteurs, ci-après l' "Utilisateur", du site https://ronde-de-l-espoir.fr/, ci-après le "Site", les présentes mentions légales.
+                <br>La connexion et la navigation sur le Site par l’Utilisateur implique l’acceptation intégrale et sans réserve des présentes mentions légales.
 
-<br><br><b>ARTICLE 1 - L'EDITEUR</b>
-<ul>
-<br><li>L’édition et la direction de la publication du Site est assurée par Jean-Christophe Darnatigues, ci-après l' "Editeur". <br>Contact : <a href="mailto:darnatigues@lamerci.com">darnatigues@lamerci.com</a></li>
-</ul>
-<br><br><b>ARTICLE 2 - L'HEBERGEUR</b>
-<ul>
-<br><li>L'hébergeur du Site est la société o2switch, accessible au lien suivant : <a href="https://www.o2switch.fr">https://www.o2switch.fr</a></li>
-</ul>
-<br><br><b>ARTICLE 3 - ACCES AU SITE</b>
-<ul> 
-<br><li>Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découlant d’une nécessité de maintenance. En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait être tenu responsable. </li>
-</ul>
-<br><br><b>ARTICLE 4 - COLLECTE DES DONNEES</b>
-<ul>
-<br><li>Tout utilisateur de ce site internet a le choix de saisir des données personnelles de manière anonyme ou non anonyme. En choisissant l'option "Je participe en tant qu'individu", l'utilisateur accepte expressément la collecte de données personnelles le concernant ainsi que la transmission de ces mêmes données à l’association DMF 34 conformément au RGPD.</li>
-<br><li>Les données personnelles collectées et transmises peuvent inclure, sans s'y limiter, le nom, l'adresse électronique, l'adresse postale, le numéro de téléphone, ou toute autre information pertinente fournie par l'utilisateur notamment dans l'objectif d'une déduction fiscale.</li>
-<br><li>En vertu du Règlement Général sur la Protection des Données (RGPD) et de la loi Informatique et Libertés du 6 janvier 1978, telle que modifiée par la loi du 20 juin 2018 relative à la protection des données personnelles, les utilisateurs disposent d'un droit d'accès, de rectification, d'opposition et de suppression de leurs données personnelles.</li>
-<br><li>Pour exercer ces droits ou pour toute question concernant la collecte et l'utilisation de vos données personnelles, vous pouvez contacter notre équipe à l'adresse suivante : <a href="mailto:rgpd@ronde-de-l-espoir.fr">rgpd@ronde-de-l-espoir.fr.</a></li>
-<br><li>Les phrases susmentionnées ne sont valables que dans le cas où l'option de formulaire "Anonyme" n'aurait pas été sélectionnée.</li>
-<br><li>La législation référant à la collecte de données présente dans cet article n'inclue pas les données récoltées sous nom de "cookies" dont la politique sur le Site est disponible dans la rubrique <a href="../cookies/">Cookies</a>.</li>
-</ul>
-<br><br><b>ARTICLE 5 - PROPRIETE INTELLECTUELLE</b> 
-<ul>
-<br><li>Le Site et son contenu sont protégés par le droit d'auteur et les droits de propriété intellectuelle en vigueur. Le Site peut contenir des liens hypertextes vers d'autres sites internet. L'Utilisateur est informé que lors de ses visites sur ces autres sites, les présentes mentions légales ne s'appliquent pas. L'Editeur ne peut être tenu pour responsable du contenu de ces sites. </li>
-</ul>
-<br><br><b>ARTICLE 6 - UTILISATION DE STRIPE</b> 
-<ul>
-<br><li>Le Site utilise Stripe, une plateforme de paiement en ligne, pour le traitement des paiements. L'Utilisateur reconnaît et accepte que ses informations de paiement soient traitées par Stripe conformément à leur politique de confidentialité disponible à l'adresse https://stripe.com/fr/privacy. </li>
-</ul>
-<br><br><b>ARTICLE 7 - LICENCE CREATIVE COMMONS</b> 
-<ul>
-<br><li>Le contenu du Site est mis à disposition selon les termes de la licence Creative Commons Attribution - Pas d'Utilisation Commerciale - Pas de Modification 4.0 International. L'Utilisateur est autorisé à partager, copier, distribuer et communiquer le contenu du site à condition de mentionner la source et de ne pas en faire un usage commercial ni de le modifier.</li>
-</ul>
-<br><br>
-</ul>
-    </p>
+                <br><br><b>ARTICLE 1 - L'EDITEUR</b>
+            <ul>
+                <br>
+                <li>L’édition et la direction de la publication du Site est assurée par Jean-Christophe Darnatigues, ci-après l' "Editeur". <br>Contact : <a href="mailto:darnatigues@lamerci.com">darnatigues@lamerci.com</a></li>
+            </ul>
+            <br><br><b>ARTICLE 2 - L'HEBERGEUR</b>
+            <ul>
+                <br>
+                <li>L'hébergeur du Site est la société o2switch, accessible au lien suivant : <a href="https://www.o2switch.fr">https://www.o2switch.fr</a></li>
+            </ul>
+            <br><br><b>ARTICLE 3 - ACCES AU SITE</b>
+            <ul>
+                <br>
+                <li>Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découlant d’une nécessité de maintenance. En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait être tenu responsable. </li>
+            </ul>
+            <br><br><b>ARTICLE 4 - COLLECTE DES DONNEES</b>
+            <ul>
+                <br>
+                <li>Tout utilisateur de ce site internet a le choix de saisir des données personnelles de manière anonyme ou non anonyme. En choisissant l'option "Je participe en tant qu'individu", l'utilisateur accepte expressément la collecte de données personnelles le concernant ainsi que la transmission de ces mêmes données à l’association DMF 34 conformément au RGPD.</li>
+                <br>
+                <li>Les données personnelles collectées et transmises peuvent inclure, sans s'y limiter, le nom, l'adresse électronique, l'adresse postale, le numéro de téléphone, ou toute autre information pertinente fournie par l'utilisateur notamment dans l'objectif d'une déduction fiscale.</li>
+                <br>
+                <li>En vertu du Règlement Général sur la Protection des Données (RGPD) et de la loi Informatique et Libertés du 6 janvier 1978, telle que modifiée par la loi du 20 juin 2018 relative à la protection des données personnelles, les utilisateurs disposent d'un droit d'accès, de rectification, d'opposition et de suppression de leurs données personnelles.</li>
+                <br>
+                <li>Pour exercer ces droits ou pour toute question concernant la collecte et l'utilisation de vos données personnelles, vous pouvez contacter notre équipe à l'adresse suivante : <a href="mailto:rgpd@ronde-de-l-espoir.fr">rgpd@ronde-de-l-espoir.fr.</a></li>
+                <br>
+                <li>Les phrases susmentionnées ne sont valables que dans le cas où l'option de formulaire "Anonyme" n'aurait pas été sélectionnée.</li>
+                <br>
+                <li>La législation référant à la collecte de données présente dans cet article n'inclue pas les données récoltées sous nom de "cookies" dont la politique sur le Site est disponible dans la rubrique <a href="../cookies/">Cookies</a>.</li>
+            </ul>
+            <br><br><b>ARTICLE 5 - PROPRIETE INTELLECTUELLE</b>
+            <ul>
+                <br>
+                <li>Le Site et son contenu sont protégés par le droit d'auteur et les droits de propriété intellectuelle en vigueur. Le Site peut contenir des liens hypertextes vers d'autres sites internet. L'Utilisateur est informé que lors de ses visites sur ces autres sites, les présentes mentions légales ne s'appliquent pas. L'Editeur ne peut être tenu pour responsable du contenu de ces sites. </li>
+            </ul>
+            <br><br><b>ARTICLE 6 - UTILISATION DE STRIPE</b>
+            <ul>
+                <br>
+                <li>Le Site utilise Stripe, une plateforme de paiement en ligne, pour le traitement des paiements. L'Utilisateur reconnaît et accepte que ses informations de paiement soient traitées par Stripe conformément à leur politique de confidentialité disponible à l'adresse https://stripe.com/fr/privacy. </li>
+            </ul>
+            <br><br><b>ARTICLE 7 - LICENCE CREATIVE COMMONS</b>
+            <ul>
+                <br>
+                <li>Le contenu du Site est mis à disposition selon les termes de la licence Creative Commons Attribution - Pas d'Utilisation Commerciale - Pas de Modification 4.0 International. L'Utilisateur est autorisé à partager, copier, distribuer et communiquer le contenu du site à condition de mentionner la source et de ne pas en faire un usage commercial ni de le modifier.</li>
+            </ul>
+            <br><br>
+            </ul>
+            </p>
         </div>
     </main>
 
